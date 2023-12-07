b8af0de2c034b27fc5f91ffdc29aa48b74d421e8
AvrAlexandra <avramescu.alexandra@gmal.com>
2023-12-07 18:26:12 +0200

    #1BetController

--- src/main/kotlin/ro/hibyte/betting/controller/BetController.kt
+++ src/main/kotlin/ro/hibyte/betting/controller/BetController.kt
@@ -1,12 +1,17 @@
+package ro.hibyte.betting.controller
 
 import org.springframework.web.bind.annotation.*
 import ro.hibyte.betting.dto.BetDTO
+import ro.hibyte.betting.dto.UserProfileDTO
+import ro.hibyte.betting.entity.Bet
+import ro.hibyte.betting.entity.UserProfile
 import ro.hibyte.betting.service.BetService
+import ro.hibyte.betting.service.UserProfileService
 
 @CrossOrigin(origins = ["http://localhost:4200"])
 @RestController
 @RequestMapping("/api/bets")
-class BetController(private val betService: BetService) {
+class BetController(private val betService: BetService, private val userProfileService: UserProfileService) {
 
     @GetMapping
     fun getAll(): Set<BetDTO> {
@@ -20,9 +25,12 @@ class BetController(private val betService: BetService) {
         return BetDTO(bet)
     }
 
-    @PostMapping
-    fun create(@RequestBody betDto: BetDTO): BetDTO {
-        val bet = betService.create(betDto)
+    @PostMapping("/{userId}")
+    fun create(@PathVariable userId: Long?, @RequestBody betDto: BetDTO): BetDTO {
+        val userProfile = userProfileService.get(userId!!)
+        val bet = betService.create(betDto, userProfile)
+        betDto.user = userProfile
+
         return BetDTO(bet)
     }
 
@@ -35,4 +43,3 @@ class BetController(private val betService: BetService) {
     @DeleteMapping("/{betId}")
     fun delete(@PathVariable betId: Long) = betService.delete(betId)
 }
-
