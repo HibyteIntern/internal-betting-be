22eed669e4eb6e99778e391e5f402be71905bab0
DanceaRadu <radu.dancea@gmail.com>
2023-12-18 17:47:40 +0200

    #6 Added option to search EventTemplates by name.

--- src/main/kotlin/ro/hibyte/betting/controller/EventTemplateController.kt
+++ src/main/kotlin/ro/hibyte/betting/controller/EventTemplateController.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ro.hibyte.betting.dto.EventTemplateRequest
 import ro.hibyte.betting.entity.EventTemplate
@@ -29,8 +30,12 @@ class EventTemplateController(private val eventTemplateService: EventTemplateSer
         eventTemplateService.getById(id)
 
     @GetMapping
-    fun getAll(): List<EventTemplate> =
-        eventTemplateService.getAll()
+    fun getAll(@RequestParam name: String?): List<EventTemplate> =
+        if(!name.isNullOrEmpty()) {
+            eventTemplateService.searchByName(name)
+        } else {
+            eventTemplateService.getAll()
+        }
 
     @PutMapping("/{id}")
     fun update(@RequestBody eventTemplateRequest: EventTemplateRequest, @PathVariable id: Long): EventTemplate =
