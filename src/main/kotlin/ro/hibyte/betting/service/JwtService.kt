3117c2ff2100d379cebc4c938bff469958c62c94
DanceaRadu <radu.dancea@gmail.com>
2024-01-12 21:12:03 +0200

    #19 Added roles to the extracted information.

--- src/main/kotlin/ro/hibyte/betting/service/JwtService.kt
+++ src/main/kotlin/ro/hibyte/betting/service/JwtService.kt
@@ -2,27 +2,30 @@ package ro.hibyte.betting.service
 
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Service
-import ro.hibyte.betting.repository.UserProfileRepository
 import com.auth0.jwt.JWT
 import com.auth0.jwt.exceptions.JWTDecodeException
-import ro.hibyte.betting.entity.UserProfile
+import ro.hibyte.betting.dto.UserPermissions
+import ro.hibyte.betting.entity.Role
 
 @Service
 class JwtService(private var userProfileService: UserProfileService) {
 
-    fun getUserProfileFromRequest(request: HttpServletRequest): UserProfile? {
+    fun getUserProfileAndPermissionsFromRequest(request: HttpServletRequest): UserPermissions? {
         val authHeader = request.getHeader("Authorization")
         return if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            extractIdFromToken(authHeader.substring(7))
+            extractInformationFromToken(authHeader.substring(7))
         } else {
             null
         }
     }
 
-    private fun extractIdFromToken(token: String): UserProfile? {
+    private fun extractInformationFromToken(token: String): UserPermissions? {
         return try {
             val decodedJWT = JWT.decode(token)
-            userProfileService.getUserProfileFromKeycloakId(decodedJWT.subject)
+            UserPermissions(
+                userProfileService.getUserProfileFromKeycloakId(decodedJWT.subject),
+                Role.USER
+            )
         } catch (e: JWTDecodeException) {
             null
         }
