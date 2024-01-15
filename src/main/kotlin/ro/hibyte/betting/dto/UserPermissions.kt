1247b921aab5fb892e46c6286040cf935de06623
DanceaRadu <radu.dancea@gmail.com>
2024-01-15 15:50:25 +0200

    #19 Removed user permissions enum.

--- src/main/kotlin/ro/hibyte/betting/dto/UserPermissions.kt
+++
@@ -1,9 +0,0 @@
-package ro.hibyte.betting.dto
-
-import ro.hibyte.betting.entity.Role
-import ro.hibyte.betting.entity.UserProfile
-
-data class UserPermissions(
-    val userProfile: UserProfile,
-    val role: Role = Role.USER
-)
