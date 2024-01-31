bd0002fd7fb2472b7e22839a98eca1fb17bf7a68
Serban681 <serbands20@gmail.com>
2024-01-31 18:27:36 +0200

    Added Leaderboard functionality

--- src/main/kotlin/ro/hibyte/betting/dto/LeaderboardDTO.kt
+++ src/main/kotlin/ro/hibyte/betting/dto/LeaderboardDTO.kt
@@ -1,7 +1,15 @@
 package ro.hibyte.betting.dto
 
+import ro.hibyte.betting.entity.UserProfile
+
 data class LeaderboardDTO(
     val id: Long,
     val name: String,
+    val leaderboardResults: List<List<UserResult>>
     // somehow the users with all their metrics scores and sorted by the desired metric
+)
+
+data class UserResult(
+    val user: UserProfileDTO,
+    val score: Int
 )
--- src/main/kotlin/ro/hibyte/betting/service/LeaderboardService.kt
+++ src/main/kotlin/ro/hibyte/betting/service/LeaderboardService.kt
@@ -1,12 +1,14 @@
 package ro.hibyte.betting.service
 
 import jakarta.transaction.Transactional
+import org.apache.catalina.User
 import org.springframework.stereotype.Service
-import ro.hibyte.betting.dto.LeaderboardConfig
-import ro.hibyte.betting.dto.LeaderboardDTO
-import ro.hibyte.betting.dto.LeaderboardRequest
+import ro.hibyte.betting.dto.*
+import ro.hibyte.betting.entity.BetType
 import ro.hibyte.betting.entity.Leaderboard
+import ro.hibyte.betting.entity.UserProfile
 import ro.hibyte.betting.exceptions.types.EntityNotFoundException
+import ro.hibyte.betting.repository.BetTypeRepository
 import ro.hibyte.betting.repository.EventRepository
 import ro.hibyte.betting.repository.LeaderboardRepository
 import ro.hibyte.betting.repository.UserProfileRepository
@@ -15,7 +17,8 @@ import ro.hibyte.betting.repository.UserProfileRepository
 class LeaderboardService(
     private val leaderboardRepository: LeaderboardRepository,
     private val eventRepository: EventRepository,
-    private val userProfileRepository: UserProfileRepository
+    private val userProfileRepository: UserProfileRepository,
+    private val betTypeRepository: BetTypeRepository
 ) {
 
     fun createLeaderboard(request: LeaderboardConfig): LeaderboardConfig {
@@ -43,12 +46,25 @@ class LeaderboardService(
         val events = leaderboard.events
         val users = leaderboard.userProfiles
 
+        val betTypes = betTypeRepository.findAll().toSet()
+
+        val results: MutableList<List<UserResult>> = mutableListOf()
+
         // compute leaderboard according to metrics from request
+        leaderboardRequest.metrics.forEach{ metric ->
+            when (metric) {
+                "numberOfBets" -> results.add(getLeaderboardMostBets(users, betTypes, leaderboardRequest.sortedBy))
+                "wonBets" -> results.add(getLeaderboardMostWins(users, betTypes, leaderboardRequest.sortedBy))
+                "fewestLosses" -> results.add(getLeaderboardFewestLosses(users, betTypes, leaderboardRequest.sortedBy))
+                //"highestEarner" -> results.add(getLeaderboardHighestEarner(users, betTypes, leaderboardRequest.sortedBy))
+            }
+
+        }
 
         return LeaderboardDTO(
             id = leaderboard.id,
             name = leaderboard.name,
-            // somehow the users with all their metrics scores and sorted by the desired metric
+            leaderboardResults = results.toList()
         )
     }
 
@@ -67,4 +83,90 @@ class LeaderboardService(
     fun deleteLeaderboard(id: Long) {
         leaderboardRepository.deleteById(id)
     }
+
+    fun getLeaderboardMostBets(users: Set<UserProfile>, betTypes: Set<BetType>, sortedBy: String): List<UserResult> {
+        return users.sortedWith(
+            compareByDescending<UserProfile> { usersByMostBetsComparator(it) }
+                .thenBy { pickComparator(sortedBy, it, betTypes) }
+            ).map { user ->
+                UserResult(
+                    user = UserProfileDTO(user),
+                    score = user.bets?.size ?: 0
+                )
+            }.toList()
+    }
+
+    fun getLeaderboardMostWins(users: Set<UserProfile>, betTypes: Set<BetType>, sortedBy: String): List<UserResult> {
+        return users.sortedWith(
+            compareByDescending<UserProfile> { usersByLeaderboardMostWinsComparator(it, betTypes) }
+            .thenBy { pickComparator(sortedBy, it, betTypes) }
+        )
+            .map{ it -> UserResult(
+                user = UserProfileDTO(it),
+                score =  usersByMostBetsComparator(it)
+            )}
+            .toList()
+    }
+
+    fun getLeaderboardFewestLosses(users: Set<UserProfile>, betTypes: Set<BetType>, sortedBy: String): List<UserResult> {
+        return users.sortedWith(
+            compareByDescending<UserProfile> { usersByFewestLossesComparator(it, betTypes) }
+                .thenBy { pickComparator(sortedBy, it, betTypes) }
+        )
+            .map{ it -> UserResult(
+                user = UserProfileDTO(it),
+                score =  usersByMostBetsComparator(it)
+            )}
+            .toList()
+    }
+
+//    fun getLeaderboardHighestEarner(users: Set<UserProfile>, betTypes: Set<BetType>, sortedBy: String): List<UserResult> {
+//        return users.sortedWith(
+//            compareByDescending<UserProfile> { usersByHighestEarnerComparator(it, betTypes) }
+//                .thenBy { pickComparator(sortedBy, it, betTypes) }
+//        )
+//            .map{ it -> UserResult(
+//                user = UserProfileDTO(it),
+//                score =  usersByMostBetsComparator(it)
+//            )}
+//            .toList()
+//    }
+
+    fun pickComparator(sortedBy: String, user: UserProfile, betTypes: Set<BetType>): Int {
+        return when (sortedBy) {
+            "numberOfBets" -> usersByMostBetsComparator(user)
+            "wonBets" -> usersByLeaderboardMostWinsComparator(user, betTypes)
+            "fewestLosses" -> usersByFewestLossesComparator(user, betTypes)
+            //"highestEarner" -> usersByHighestEa
+            else -> 0
+        }
+    }
+
+    fun usersByMostBetsComparator(user: UserProfile): Int {
+        return user.bets?.size ?: 0
+    }
+
+    fun usersByLeaderboardMostWinsComparator(user: UserProfile, betTypes: Set<BetType>): Int {
+        return user.bets?.count { bet ->
+            bet.value == betTypes.find { it.id == bet.betType?.id }?.finalOutcome
+        } ?: 0
+    }
+
+    fun usersByFewestLossesComparator(user: UserProfile, betTypes: Set<BetType>): Int {
+        return user.bets?.count { bet ->
+            bet.value != betTypes.find { it.id == bet.betType?.id }?.finalOutcome
+        } ?: 0
+    }
+
+//    fun usersByHighestEarnerComparator(user: UserProfile, betTypes: Set<BetType>): Int {
+//        return user.bets?.sumOf { bet ->
+//            if (bet.value == betTypes.find { it.id == bet.betType?.id }?.finalOutcome) {
+//                bet.amount * bet.odds
+//            } else {
+//                0
+//            }
+//        }?.toInt() ?: 0
+//    }
 }
+
+
